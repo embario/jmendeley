9b542772747d16c4095c0209c2d7ee2afe76c099
mbarrenecheajr <mbarrenecheajr@gmail.com>
2012-05-03 08:31:20 -0600

    -more work on the UI.

--- JMendeley/src/main/SearchResultsTable.java
+++ JMendeley/src/main/SearchResultsTable.java
@@ -1,45 +1,7 @@
 package main;
-
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
- 
-/*
- * TableDemo.java requires no other files.
- */
  
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -47,22 +9,14 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
-import main.TableRenderDemo.MyTableModel;
-
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
  
-/** 
- * TableDemo is just like SimpleTableDemo, except that it
- * uses a custom TableModel.
- */
 @SuppressWarnings("serial")
 public class SearchResultsTable extends JPanel {
-	
-    private boolean DEBUG = false;
     
     private ArrayList <Paper> papers = null;
     private JTable _table = null;
@@ -141,8 +95,7 @@ public class SearchResultsTable extends JPanel {
      * @author mbarrenecheajr
      *
      */
-    @SuppressWarnings("serial")
-	class SearchResultsModel extends AbstractTableModel {
+    class SearchResultsModel extends AbstractTableModel {
   
     	private JTable table = null;
     	private ButtonColumn buttonColumn = null;
